5c182d5f6a58e2b7c775b3151b55067ed95d586b
Dushimepaulette1 <p.dushime1@alustudent.com>
2026-03-12 20:12:28 +0200

    exercise 5 done

--- exercise-5.py
+++ exercise-5.py
@@ -4,3 +4,9 @@
 #Create a function called repeat. 
 # It takes two parameters, a string and a count, and returns a new string that is the old one repeated count times.
 # ============================================================================
+def repeat(words, count):
+    newWord = ""
+    for word in range(count):
+         newWord += words
+    return newWord
+print(repeat("paulette\n", 5))
